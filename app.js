00acd9c204750bbb6904f02ef6f141455e5f500e
Jeremy Christian <jeremy.l.christian@gmail.com>
2023-06-12 11:09:45 -0700

    Number buttons put their value in an array. Log all number values to console for testing purposes.

--- app.js
+++ app.js
@@ -16,3 +16,56 @@ let subtract = document.getElementById('subtract');
 let multiply = document.getElementById('multiply');
 let divide = document.getElementById('divide');
 
+let firstNumArr = [];
+
+let secondNumArr = [];
+
+one.addEventListener('click', () => {
+    firstNumArr.push(one.value);
+    console.log(one.value);
+});
+
+two.addEventListener('click', () => {
+    firstNumArr.push(two.value);
+    console.log(two.value);
+});
+
+three.addEventListener('click', () => {
+    firstNumArr.push(three.value);
+    console.log(three.value);
+});
+
+four.addEventListener('click', () => {
+    firstNumArr.push(four.value);
+    console.log(four.value);
+});
+
+five.addEventListener('click', () => {
+    firstNumArr.push(five.value);
+    console.log(five.value);
+});
+
+six.addEventListener('click', () => {
+    firstNumArr.push(six.value);
+    console.log(six.value);
+});
+
+seven.addEventListener('click', () => {
+    firstNumArr.push(seven.value);
+    console.log(seven.value);
+});
+
+eight.addEventListener('click', () => {
+    firstNumArr.push(eight.value);
+    console.log(eight.value);
+});
+
+nine.addEventListener('click', () => {
+    firstNumArr.push(nine.value);
+    console.log(nine.value);
+});
+
+zero.addEventListener('click', () => {
+    firstNumArr.push(zero.value);
+    console.log(zero.value);
+});
