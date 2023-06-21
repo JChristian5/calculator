a189cc1a6d85afdf6d305a117bb8e857e08f2611
Jeremy Christian <jeremy.l.christian@gmail.com>
2023-06-21 13:12:18 -0700

    Zero can not be first number in calculator (array)

--- app.js
+++ app.js
@@ -30,53 +30,58 @@ one.addEventListener('click', () => {
 two.addEventListener('click', () => {
     firstNumArr.push(two.value);
     console.log(two.value);
-    displayBox.textContent = firstNumArr.join("")
+    displayBox.textContent = firstNumArr.join("");
 });
 
 three.addEventListener('click', () => {
     firstNumArr.push(three.value);
     console.log(three.value);
-    displayBox.textContent = firstNumArr.join("")
+    displayBox.textContent = firstNumArr.join("");
 });
 
 four.addEventListener('click', () => {
     firstNumArr.push(four.value);
     console.log(four.value);
-    displayBox.textContent = firstNumArr.join("")
+    displayBox.textContent = firstNumArr.join("");
 });
 
 five.addEventListener('click', () => {
     firstNumArr.push(five.value);
     console.log(five.value);
-    displayBox.textContent = firstNumArr.join("")
+    displayBox.textContent = firstNumArr.join("");
 });
 
 six.addEventListener('click', () => {
     firstNumArr.push(six.value);
     console.log(six.value);
-    displayBox.textContent = firstNumArr.join("")
+    displayBox.textContent = firstNumArr.join("");
 });
 
 seven.addEventListener('click', () => {
     firstNumArr.push(seven.value);
     console.log(seven.value);
-    displayBox.textContent = firstNumArr.join("")
+    displayBox.textContent = firstNumArr.join("");
 });
 
 eight.addEventListener('click', () => {
     firstNumArr.push(eight.value);
     console.log(eight.value);
-    displayBox.textContent = firstNumArr.join("")
+    displayBox.textContent = firstNumArr.join("");
 });
 
 nine.addEventListener('click', () => {
     firstNumArr.push(nine.value);
     console.log(nine.value);
-    displayBox.textContent = firstNumArr.join("")
+    displayBox.textContent = firstNumArr.join("");
 });
 
 zero.addEventListener('click', () => {
     firstNumArr.push(zero.value);
     console.log(zero.value);
-    displayBox.textContent = firstNumArr.join("")
+    if(firstNumArr[0] == 0) {
+        firstNumArr.shift();
+        displayBox.textContent = 0;
+    } else {
+        displayBox.textContent = firstNumArr.join("");
+    }
 });
