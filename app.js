f5733ecfeddbcee22b396a6d51b639f2fdc8d804
Jeremy Christian <jeremy.l.christian@gmail.com>
2023-06-22 09:58:31 -0700

    Can not add extra zero. Ex: 00, will just stay 0. Can not add zero then number. Ex: 01 displays 1.

--- app.js
+++ app.js
@@ -34,75 +34,118 @@ one.addEventListener('click', () => {
     //     console.log(one.value);
     //     bottomText.textContent = secondNumArr.join("");
     // } else {
-        firstNumArr.push(one.value);
-        console.log(one.value);
-        bottomText.textContent = firstNumArr.join("");
+    firstNumArr.push(one.value);
+    console.log(one.value);
+    bottomText.textContent = firstNumArr.join("");
     // }
+    if(firstNumArr[0] == 0 && firstNumArr[1] != '.') {
+        firstNumArr.shift();
+        bottomText.textContent = firstNumArr.join("");
+    }
 });
 
 two.addEventListener('click', () => {
     firstNumArr.push(two.value);
     console.log(two.value);
     bottomText.textContent = firstNumArr.join("");
+    if(firstNumArr[0] == 0 && firstNumArr[1] != '.') {
+        firstNumArr.shift();
+        bottomText.textContent = firstNumArr.join("");
+    }
 });
 
 three.addEventListener('click', () => {
     firstNumArr.push(three.value);
     console.log(three.value);
     bottomText.textContent = firstNumArr.join("");
+    if(firstNumArr[0] == 0 && firstNumArr[1] != '.') {
+        firstNumArr.shift();
+        bottomText.textContent = firstNumArr.join("");
+    }
 });
 
 four.addEventListener('click', () => {
     firstNumArr.push(four.value);
     console.log(four.value);
     bottomText.textContent = firstNumArr.join("");
+    if(firstNumArr[0] == 0 && firstNumArr[1] != '.') {
+        firstNumArr.shift();
+        bottomText.textContent = firstNumArr.join("");
+    }
 });
 
 five.addEventListener('click', () => {
     firstNumArr.push(five.value);
     console.log(five.value);
     bottomText.textContent = firstNumArr.join("");
+    if(firstNumArr[0] == 0 && firstNumArr[1] != '.') {
+        firstNumArr.shift();
+        bottomText.textContent = firstNumArr.join("");
+    }
 });
 
 six.addEventListener('click', () => {
     firstNumArr.push(six.value);
     console.log(six.value);
     bottomText.textContent = firstNumArr.join("");
+    if(firstNumArr[0] == 0 && firstNumArr[1] != '.') {
+        firstNumArr.shift();
+        bottomText.textContent = firstNumArr.join("");
+    }
 });
 
 seven.addEventListener('click', () => {
     firstNumArr.push(seven.value);
     console.log(seven.value);
     bottomText.textContent = firstNumArr.join("");
+    if(firstNumArr[0] == 0 && firstNumArr[1] != '.') {
+        firstNumArr.shift();
+        bottomText.textContent = firstNumArr.join("");
+    }
 });
 
 eight.addEventListener('click', () => {
     firstNumArr.push(eight.value);
     console.log(eight.value);
     bottomText.textContent = firstNumArr.join("");
+    if(firstNumArr[0] == 0 && firstNumArr[1] != '.') {
+        firstNumArr.shift();
+        bottomText.textContent = firstNumArr.join("");
+    }
 });
 
 nine.addEventListener('click', () => {
     firstNumArr.push(nine.value);
     console.log(nine.value);
     bottomText.textContent = firstNumArr.join("");
+    if(firstNumArr[0] == 0 && firstNumArr[1] != '.') {
+        firstNumArr.shift();
+        bottomText.textContent = firstNumArr.join("");
+    }
 });
 
 zero.addEventListener('click', () => {
     firstNumArr.push(zero.value);
     console.log(zero.value);
-    if(firstNumArr[0] == 0) {
-        firstNumArr.shift();
-        bottomText.textContent = 0;
+    if(firstNumArr[0] == 0 && firstNumArr[1] == 0) {
+        firstNumArr.pop();
     } else {
         bottomText.textContent = firstNumArr.join("");
     }
 });
 
 decimal.addEventListener('click', () => {
-    firstNumArr.push('.');
-    bottomText.textContent = firstNumArr.join("");
-    decimal.disabled = true;
+    if(firstNumArr == []) {
+        firstNumArr.push(0);
+        firstNumArr.push('.');
+        decimal.disabled = true;
+    // } else if(firstNumArr[0] == 0 && firstNumArr[1] != '.') {
+    //     firstNumArr.shift();        
+    } else {
+        firstNumArr.push('.');
+        bottomText.textContent = firstNumArr.join("");
+        decimal.disabled = true;
+    }
 });
 
 add.addEventListener('click', () => {
